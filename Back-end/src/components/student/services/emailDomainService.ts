2a1e20c648ed16fbb1d4047989c7b25af279d1fd
buonnguwaaa <nhannguyen04.it@gmail.com>
2025-03-26 17:04:09 +0700

    add crud for email domain

--- Back-end/src/components/student/services/emailDomainService.ts
+++ Back-end/src/components/student/services/emailDomainService.ts
@@ -2,6 +2,44 @@ import EmailDomain, { IEmailDomain } from '../models/EmailDomain'
 
 class EmailDomainService {
     /**
+     * Thêm domain email được phép đăng ký
+     * @param domain domain cần thêm
+     * @returns Promise<IEmailDomain> Domain đã thêm
+     */
+    async addAllowedEmailDomain(domain: string): Promise<IEmailDomain> {
+        const newDomain = new EmailDomain({ domain })
+        return await newDomain.save()
+    }
+    /**
+     * Xóa domain email đã được đăng ký
+     * @param domain domain cần xóa
+     * @returns Promise<IEmailDomain> Domain đã xóa
+     */
+    async deleteAllowedEmailDomain(domain: string): Promise<IEmailDomain> {
+        const result = EmailDomain.findOneAndDelete({ domain })
+        if (!result) {
+            throw new Error('Domain not found')
+        }
+        return result
+    }
+    /**
+     * Update domain email đã được đăng ký
+     * @param domain 
+     * @param newDomain 
+     * @returns Promise<IEmailDomain> Domain đã update
+     */
+    async updateAllowedEmailDomain(domain: string, newDomain: string): Promise<IEmailDomain> {
+        const updatedDomain = await EmailDomain.findOneAndUpdate(
+            { domain },
+            { domain: newDomain },
+            { new: true }
+        )
+        if (!updatedDomain) {
+            throw new Error('Domain not found')
+        }
+        return updatedDomain
+    }
+    /**
      * Lấy danh sách các email domain được phép đăng ký
      * @returns {Promise<IEmailDomain[]>} Danh sách các domain
      */
--- Back-end/src/routes/index.ts
+++ Back-end/src/routes/index.ts
@@ -3,6 +3,7 @@ import studentRouter from '../components/student/routes/studentRoute';
 import facultyRouter from '../components/faculty/routes/facultyRoute';
 import programRouter from '../components/program/routes/programRoute';
 import statusRouter from '../components/status/routes/statusRoute';
+import emailDomainRouter from '../components/student/routes/emailDomainRoute';
 /**
  * Cấu hình tất cả các routes cho ứng dụng
  * @param app Express application instance
@@ -12,6 +13,7 @@ function route(app: Express): void {
   app.use('/api/faculties', facultyRouter);
   app.use('/api/programs', programRouter);
   app.use('/api/statuses', statusRouter);
+  app.use('/api/email-domains', emailDomainRouter);
 }
 
 export default route;
